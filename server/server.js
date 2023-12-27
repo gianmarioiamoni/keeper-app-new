8a664f609f6baf4af46ca618e0056d32c249f7e8
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2023-12-27 01:56:41 +0100

    first version with delete fixed

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("../public"));
+app.use(express.static("../client/keeper-app/public"));
 
 
 // const for DB
@@ -29,12 +29,9 @@ const noteSchema = new mongoose.Schema(
     {
         title: String,
         content: String,
-        id: String 
     }
 );
 
-
-  
 // create a new Model
 const Note = mongoose.model("Note", noteSchema);
 
@@ -44,29 +41,30 @@ const Note = mongoose.model("Note", noteSchema);
 ////
 
 app.route("/notes")
-.get(function(req, res) {
-   Note.find({}, null)
-   .then( docs => res.send(docs) )
-   .catch( err => res.send(err) );
+    .get(async function (req, res) {
+        try {
+            const docs = await Note.find({}, null);
+            res.send(docs);
+        } catch (err) {
+            res.send(err);
+        }
     
 })
-.post(function(req, res) {
-    const ind = req.body.id;
-    console.log("ind = " + ind);
-    new Note({title: req.body.title, content: req.body.content, id: req.body.id}).save()
-    //Note.create({title: req.body.title, content: req.body.content, id: "iddddd"})
-    .then( (savedDoc) => {
-         const newId = (savedDoc._id.toString()); 
-         return res.status(201).json({
-            success: true,
-            id: newId,
-            message: 'Note created!'
-         })
+    .post(async function (req, res) {
+        // const id = req.body.id;
+        try {
+            const savedDoc = await new Note({ title: req.body.title, content: req.body.content }).save()
+            const newId = (savedDoc._id.toString());
+            return res.status(201).json({
+                success: true,
+                id: newId,
+                message: 'Note created!'
+            })
+        } catch (err) {
+            console.log(err);
+        }
+
     })
-    .catch( (err) => { 
-         console.log(err);
-    });
-})
 
 
 ////////////
@@ -74,30 +72,37 @@ app.route("/notes")
 //
 ////
 app.route("/notes/:id")
-.put(function(req, res) {
-    console.log("**** app.route().put");
-    console.log(req.body.title + " " + req.body.content + " " + req.body.oldTitle + " " + req.body.oldContent);
-    console.log("id = " + req.params.id);
-    Note.findOneAndReplace(
-        { $and: [ { title: req.body.oldTitle }, { content: req.body.oldContent } ] },
-        {
-            title: req.body.title,
-            content: req.body.content,
-            id: req.params.id 
+    .put(async function (req, res) {
+        console.log("**** app.route().put");
+        console.log(req.body.title + " " + req.body.content + " " + req.body.oldTitle + " " + req.body.oldContent);
+        console.log("id = " + req.params.id);
+        // Note.findOneAndReplace(
+        // { $and: [ { title: req.body.oldTitle }, { content: req.body.oldContent } ] },
+        try {
+            await Note.findByIdAndUpdate((req.params.id),
+                {
+                    title: req.body.title,
+                    content: req.body.content,
+                    id: req.params.id
+                }
+            )
+            res.send("note successfully replaced");
+        } catch (err) {
+            res.send(err);
         }
-    )
-    .then( () => res.send("note successfully replaced"))
-    .catch( err => res.send(err) )
-})
-.delete(function(req, res) {
-    Note.findOneAndDelete(
-        { $and: [ { title: req.body.title }, { content: req.body.content } ] } 
-    )
-    .then( () => res.send("note successfully deleted"))
-    .catch( err => res.send(err) )
+    })
+.delete(async function(req, res) {
+    // Note.findOneAndDelete(
+    // { $and: [ { title: req.body.title }, { content: req.body.content } ] }
+    console.log("deleting note id = " + req.params.id);
+    try {
+        await Note.findByIdAndDelete((req.params.id));
+        res.send("note successfully deleted");
+    } catch (err) {
+        err => res.send(err)
+    }
     
 });
-
 
 
 app.listen(5000, () => {
